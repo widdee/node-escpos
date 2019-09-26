0ee46f7c7d6cedb25846a4952611ad9cb4789ffc
Ibrahim <nibrahimb1993@gmail.com>
2019-09-26 18:45:27 +0300

    remove required adaptors

--- index.js
+++ index.js
@@ -2,19 +2,16 @@
 /**
  * Adapters
  */
-exports.USB     = require('./adapter/usb');
-exports.Serial  = require('./adapter/serial');
 exports.Network = require('./adapter/network');
 exports.Console = require('./adapter/console');
-exports.Bluetooth = require('./adapter/bluetooth');
 
 /**
  * Printer Supports
  */
-exports.Image    = require('./image');
-exports.Server   = require('./server');
-exports.Printer  = require('./printer');
-exports.Screen  = require('./screen');
-exports.Adapter  = require('./adapter');
-exports.command  = require('./commands');
+exports.Image = require('./image');
+exports.Server = require('./server');
+exports.Printer = require('./printer');
+exports.Screen = require('./screen');
+exports.Adapter = require('./adapter');
+exports.command = require('./commands');
 exports.Printer2 = require('./promiseify');

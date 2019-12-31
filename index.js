c27802ee493dc2fe4254306fe4ce62da45f99bac
Mortada AlKhars <mortadaajk@gmail.com>
2019-12-31 22:04:43 +0300

    add usb adapter

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-'use strict';
+"use strict";
 /**
  * Adapters
  */
-exports.USB     = require('./adapter/usb');
-exports.Serial  = require('./adapter/serial');
-exports.Network = require('./adapter/network');
-exports.Console = require('./adapter/console');
-exports.Bluetooth = require('./adapter/bluetooth');
-exports.RawBT   = require('./adapter/rawbt');
+exports.USB = require("./adapter/usb");
+exports.Network = require("./adapter/network");
+exports.Console = require("./adapter/console");
 
 /**
  * Printer Supports
  */
-exports.Image    = require('./image');
-exports.Server   = require('./server');
-exports.Printer  = require('./printer');
-exports.Screen  = require('./screen');
-exports.Adapter  = require('./adapter');
-exports.command  = require('./commands');
-exports.Printer2 = require('./promiseify');
+exports.Image = require("./image");
+exports.Server = require("./server");
+exports.Printer = require("./printer");
+exports.Screen = require("./screen");
+exports.Adapter = require("./adapter");
+exports.command = require("./commands");
+exports.Printer2 = require("./promiseify");
